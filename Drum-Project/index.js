52ae01ed15cb09b04eb853f1eebf5c0df80cf829
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-01-31 15:41:19 -0300

    Add background that is not working somehow.

--- Drum-Project/index.js
+++ Drum-Project/index.js
@@ -1,6 +1,13 @@
 window.addEventListener("keydown", function(e){
     console.log (e.keyCode)
     let audio = document.getElementById (e.keyCode)
+    let key = document.getElementById (`k${e.keyCode}`)
     if(!audio){return;}
-    audio.play()
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+    this.setTimeout (function(){
+    key.classList.remove('playing')
+}, 300);
 })
+
